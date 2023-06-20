f5d8153c94c991e4818f01ed62f943deb6f39e98
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2023-06-20 19:59:00 +0530

    Implement octynectl --version, --help

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@ fn main() {
     let mut args = std::env::args().collect::<Vec<String>>();
     args.remove(0);
 
+    // Parse top-level options.
+    let top_level_opts_map = options::parse_options(&mut args, true);
+    if top_level_opts_map.contains_key("v") || top_level_opts_map.contains_key("version") {
+        println!("octynectl {}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+    if top_level_opts_map.contains_key("h") || top_level_opts_map.contains_key("help") {
+        // TODO: Send help for specific subcommands down the line?
+        println!("{}", help::HELP_STR);
+        return;
+    }
+
+    // Check for commands.
     if args.len() == 0 {
         println!("{}", help::invalid_usage(help::INCORRECT_USAGE, ""));
         return;
     }
-
-    // Parse top-level options.
-    let top_level_opts_map = options::parse_options(&mut args, true);
 
     // Parse subcommand.
     let subcommand_tmp = args[0].clone();
