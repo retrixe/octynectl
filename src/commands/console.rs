1847b985e5fd6f3d328aa0e0f21d5ce9f02e43dd
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-01-25 11:17:39 +0530

    Write initial console command, not interactive yet

--- src/commands/console.rs
+++ src/commands/console.rs
@@ -1,18 +1,11 @@
-use std::fmt::Write;
-use std::{collections::HashMap, env, process::exit};
+use std::{collections::HashMap, process::exit};
 
 use crate::api::server::connect_to_server_console;
-use futures_util::StreamExt;
-use minus::MinusError;
-use tokio_tungstenite::tungstenite::protocol::{frame::coding::CloseCode, CloseFrame};
-
-fn minus_page_lines(lines: &str) -> Result<(), MinusError> {
-    let mut output = minus::Pager::new();
-    output.set_run_no_overflow(true)?;
-    writeln!(output, "{}", lines)?;
-    minus::page_all(output)?;
-    Ok(())
-}
+use futures_util::{SinkExt, StreamExt};
+use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
+use tokio_tungstenite::tungstenite::protocol::CloseFrame;
+use tokio_tungstenite::tungstenite::Message;
+use tokio_util::codec::{FramedRead, LinesCodec};
 
 pub async fn console_cmd(args: Vec<String>, top_level_opts: HashMap<String, String>) {
     let mut args = args.clone();
@@ -27,13 +20,12 @@ pub async fn console_cmd(args: Vec<String>, top_level_opts: HashMap<String, Stri
     } else if args.len() != 2 {
         println!(
             "{}",
-            crate::help::invalid_usage(crate::help::INCORRECT_USAGE, "logs")
+            crate::help::invalid_usage(crate::help::INCORRECT_USAGE, "console")
         );
         exit(1);
     }
-    let no_pager = opts.contains_key("no-pager") || env::var("NOPAGER").eq(&Ok("true".to_string()));
-    let use_minus =
-        opts.contains_key("use-builtin-pager") || env::var("PAGER").eq(&Ok(String::new()));
+    let no_interactive = opts.contains_key("no-interactive") // --no-interactive is set
+        || !atty::is(atty::Stream::Stdout); // TTY is not present
 
     // Connect to WebSocket over Unix socket
     let socket = connect_to_server_console(args[1].clone())
@@ -42,64 +34,93 @@ pub async fn console_cmd(args: Vec<String>, top_level_opts: HashMap<String, Stri
             println!("Error: {}", e);
             exit(1);
         });
-
-    // FIXME: Implement proper `console` command, everything so far is just copy paste from `logs`..
-    // Split the socket and then read a single message from it
     let (write, read) = socket.split();
-    let (item, read) = read.into_future().await;
-    let item = item
-        .unwrap_or_else(|| {
-            println!("Error: Received no message from Octyne!");
-            exit(1);
-        })
-        .unwrap_or_else(|e| {
-            println!("Error: {}", e);
+
+    // The threads don't close each other because if the read thread errors, it's unlikely writing
+    // a close message will work, and vice versa? TODO: But then the read thread doesn't get flushed
+    // Construct pager
+    /* let mut output = if no_interactive {
+        Option::Some(minus::Pager::new())
+    } else {
+        Option::None
+    }; */
+    // Create read thread
+    // let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+    tokio::spawn(async move {
+        let mut read = read;
+        while let Some(item) = read.next().await {
+            let item = item.unwrap_or_else(|e| {
+                println!("Read error: {}", e);
+                exit(1);
+            });
+            if item.is_close() {
+                println!("Read error: Received close message from Octyne!");
+                exit(1);
+            }
+            let item = item.to_text().unwrap_or_else(|e| {
+                println!("Read error: {}", e);
+                exit(1);
+            });
+            if no_interactive {
+                println!("{}", item);
+                continue;
+            }
+            // FIXME: Interactive session
+            /* minus_page_lines(item).unwrap_or_else(|e| {
+                println!("Error: {}", e);
+                exit(1);
+            }); */
+            println!("{}", item);
+        }
+        // tx.send(()).unwrap(); // Non-issue, this channel just ensures read is flushed before exit
+    });
+    // Create write thread
+    let mut write = write;
+    let stdin = tokio::io::stdin();
+    let mut stdin = FramedRead::new(stdin, LinesCodec::new());
+    while let Some(line) = stdin.next().await {
+        let line = line.unwrap_or_else(|e| {
+            println!("Write error: {}", e);
             exit(1);
         });
-    if item.is_close() {
-        println!("Error: Received close message from Octyne!");
-        exit(1);
+        if line.is_empty() {
+            continue;
+        }
+        write.send(Message::Text(line)).await.unwrap_or_else(|e| {
+            println!("Write error: {}", e);
+            exit(1);
+        });
     }
-    let item = item.to_text().unwrap_or_else(|e| {
-        println!("Error: {}", e);
-        exit(1);
-    });
-
-    // Close the WebSocket connection.
-    let mut socket = read.reunite(write).unwrap_or_else(|e| {
-        println!("Error: {}", e);
-        exit(1);
-    });
-    socket
-        .close(Some(CloseFrame {
+    // Gracefully exit on EOF
+    write
+        .send(Message::Close(Some(CloseFrame {
             code: CloseCode::Normal,
             reason: "Done".into(),
-        }))
+        })))
         .await
         .unwrap_or_else(|e| {
             println!("Error: {}", e);
             exit(1);
         });
-
-    // Log the output.
-    if no_pager {
-        return println!("{}", item);
-    }
-    #[cfg(target_family = "unix")]
-    if !use_minus {
-        pager::Pager::with_default_pager("less").setup();
-        println!("{}", item);
-        exit(0);
-    }
-    minus_page_lines(item).unwrap_or_else(|e| {
+    write.close().await.unwrap_or_else(|e| {
         println!("Error: {}", e);
         exit(1);
     });
+    // Wait for the read thread to exit
+    /* rx.await.unwrap_or_else(|e| {
+        println!("Error: {}", e);
+        exit(1);
+    }); */
+    exit(0);
 }
 
 pub fn console_cmd_help() {
     println!(
         "Interact with an app's console and send input.
+
+This opens an interactive terminal UI, which can be disabled using the
+`--no-interactive` flag. The interactive UI will be disabled automatically if
+the command output is not being sent to a TTY (terminal) session.
 
 If you only want the app's output logs, and don't want to send any input to it,
 use the `logs` command instead.
